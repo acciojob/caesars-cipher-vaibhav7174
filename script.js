41d75de0f40b8ba7d0fb3007d76404ec1e4be295
Vaibhav Humbade <117819312+vaibhav7174@users.noreply.github.com>
2023-04-03 09:55:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
-// Your Script here.
-var lookup = {
-  'A': 'N','B': 'O','C': 'P','D': 'Q',
-  'E': 'R','F': 'S','G': 'T','H': 'U',
-  'I': 'V','J': 'W','K': 'X','L': 'Y',
-  'M': 'Z','N': 'A','O': 'B','P': 'C',
-  'Q': 'D','R': 'E','S': 'F','T': 'G',
-  'U': 'H','V': 'I','W': 'J','X': 'K',
-  'Y': 'L','Z': 'M'
-};
-
-function rot13(encodedStr) {
-  var codeArr = encodedStr.split("");  // String to Array
-  var decodedArr = []; // Your Result goes here
-  // Only change code below this line
-
-  decodedArr = codeArr.map(function(letter) {
-    if(lookup.hasOwnProperty(letter)) {
-      letter = lookup[letter];
-    }
-    return letter;
-  });
-
-  // Only change code above this line
-  return decodedArr.join(""); // Array to String
+function rot13(str) {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    return str
+      .split('')
+      .map(char => {  
+        const pos = alphabet.indexOf(char);      
+        return pos >= 0 ? alphabet[(pos + 13) % 26] : char;
+      })
+      .join('');
 }
