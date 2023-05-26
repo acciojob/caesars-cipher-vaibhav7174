22de0534f3636d0f37cd60d72a557ecb66e229ed
Vaibhav Humbade <117819312+vaibhav7174@users.noreply.github.com>
2023-05-26 12:44:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-var lookup = {
+const lookup = {
   'A': 'N','B': 'O','C': 'P','D': 'Q',
   'E': 'R','F': 'S','G': 'T','H': 'U',
   'I': 'V','J': 'W','K': 'X','L': 'Y',
   'M': 'Z','N': 'A','O': 'B','P': 'C',
   'Q': 'D','R': 'E','S': 'F','T': 'G',
   'U': 'H','V': 'I','W': 'J','X': 'K',
-  'Y': 'L','Z': 'M'
+  'Y': 'L','Z': 'M', '?': '?', ',': ','
 };
 
-function rot13(encodedStr) {
-  var codeArr = encodedStr.split("");  // String to Array
-  var decodedArr = []; // Your Result goes here
+function rot13(encodedStr){
+	let decodedArr = []; // Your Result goes here
   // Only change code below this line
-
-  decodedArr = codeArr.map(function(letter) {
-    if(lookup.hasOwnProperty(letter)) {
-      letter = lookup[letter];
-    }
-    return letter;
-  });
-
-  // Only change code above this line
-  return decodedArr.join(""); // Array to String
+	for(let i=0;i<encodedStr.length;i++){
+		if(lookup[encodedStr[i]] === undefined){
+			decodedArr.push(encodedStr[i])
+		}
+		else{
+			decodedArr.push(lookup[encodedStr[i]])
+		}
+	}
+  return decodedArr.join('');//return decodedArr
 }
 
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+
+// console.log(rot13("SERR YBIR? NPPVBWBO"));
